336790793e201c0705fbfc8cd43a17d2a11d1892
Anamul <anamulhaquemohan@gmail.com>
2016-11-03 20:45:54 +0600

    updated script files

--- js/main.js
+++ js/main.js
@@ -1,1 +1,53 @@
+/*------------------------------------------------------------------
 
+[Main Script]
+
+Project:    PortfolioS
+Version:    1.0
+Author:     anamulhaquemohan
+Author URL: http://themeforest.net/user/anamulhaquemohan
+
+-------------------------------------------------------------------*/
+
+;(function ($) {
+    "use strict";
+    
+    /* -------------------------------
+       Common Variables
+       ------------------------------- */
+    var $wn = $(window);
+  
+    /* -------------------------------
+       Scroll Animations
+       ------------------------------- */
+    var scrollAnimations = function () {
+        ScrollReveal().reveal('.Reveal', { duration: 800, mobile: false });
+    };
+
+    /* -------------------------------
+       Function calls
+       ------------------------------- */
+    $(function () {
+        /* Portfolio Items */
+        var $portfolio = $('#portfolio'),
+            $portfolioFMenu = $portfolio.find('.portfolio--filter-menu'),
+            $portfolioItems = $portfolio.find('.portfolio--items'),
+            $portfolioItem = $portfolioItems.children('.portfolio--item');
+        
+        $portfolioItems.isotope();
+        
+        $portfolioFMenu.on('click', 'li', function () {
+            var $t = $(this),
+                f = $t.data('target'),
+                s = (f !== '*') ? '[data-filter~="'+ f +'"]' : f;
+            
+            $portfolioItems.isotope({
+                filter: s
+            });
+            
+            $t.addClass('active').siblings().removeClass('active');
+        });
+    });
+    
+    $wn.on('load', scrollAnimations);
+})(jQuery);
